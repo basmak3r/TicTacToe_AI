1e7f4ef370c104c4ba822488e3b10b316a8cbb9d
basmak3r <basmak3r@gmail.com>
2021-08-22 13:59:25 +0530

    minimax

--- Game.py
+++ Game.py
@@ -1,3 +1,4 @@
+import time
 game=True
 x="X"
 X=False
@@ -11,6 +12,77 @@ a=[[]]
 
 print("")
 print("")
+
+def evaluate(a):
+    for i in range(3):
+        if(a[0][i]==a[1][i]==a[2][i]==X):
+            return 10
+        if(a[0][i]==a[1][i]==a[2][i]==(not X)):
+            return -10
+        if(a[i][0]==a[i][1]==a[i][2]==(not X)):
+            return -10
+        if(a[i][0]==a[i][1]==a[i][2]==X):
+            return 10
+    if(a[0][0]==a[1][1]==a[2][2]):
+        if(a[0][0]==X):
+            return 10
+        elif(a[0][0]==(not X)):
+            return -10
+    if(a[0][2]==a[1][1]==a[2][0]):
+        if(a[0][2]==X):
+            return 10
+        elif(a[0][2]==(not X)):
+            return -10
+        
+
+def minimax(a,depth,isMax):
+    score=evaluate(a)
+    if score==10 or score==-10:
+        return score
+    # if(Left(a)==False):
+    #     return 0
+    if(isMax):
+        best=-1000
+        for i in range(3):
+            for j in range(3):
+                if(a[i][j]==-1):
+                    a[i][j]=X
+                    best=max(best,minimax(a,depth+1,not isMax))
+                    a[i][j]=-1
+        return best
+    else:
+        best=1000
+        for i in range(3):
+            for j in range(3):
+                if(a[i][j]==-1):
+                    a[i][j]=not X
+                    # time.sleep(2)
+                    # print(a)
+                    best=min(best,minimax(a,depth+1,not isMax))
+                    a[i][j]=-1
+        print(best)
+        return best
+        
+    
+
+
+def bestmove(a):
+    bestval=-1000
+    move=(-1,-1)
+    for i in range(3):
+        for j in range(3):
+            if(a[i][j]==-1):
+                a[i][j]=X
+                moveval=minimax(a,0,False)
+                print(a)
+                a[i][j]=-1
+                time.sleep(2)
+                print(moveval)
+                if(bestval<moveval):
+                    move=(i,j)
+                    bestval=moveval
+    return move
+        
 for i in range(3):
     
     a[i].append(-1)
@@ -59,11 +131,16 @@ while(game):
     elif(n<9):
         a[2][n%3]=X
     
-    X=not X
+    X=(not X)
 
     
     print_array()
     print("")
+    print(bestmove(a))
+    game=False
+    print(a)
+
+
 
 print("Fun-----------")
 
